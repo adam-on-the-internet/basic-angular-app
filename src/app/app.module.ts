1553cf6e459809f96e521f1861e9ac4fd1aa7ac2
Adam Callanan <a.callanan@sbcglobal.net>
2020-01-15 18:02:52 -0600

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { InfoComponent } from "./components/info/info.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
