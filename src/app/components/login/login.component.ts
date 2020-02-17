bab1e61c924bf6fc564862bb6f4a53024c4b09f8
Adam Callanan <a.callanan@sbcglobal.net>
2020-02-16 21:59:25 -0600

    starting login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  public email: string = null;
+  public password: string = null;
 
-  constructor() { }
+  public showErrors = false;
 
-  ngOnInit() {
+  public get errors(): string[] {
+    const myErrors = [];
+    if (this.passwordInvalid) {
+      myErrors.push("Please provide a password.");
+    }
+    if (this.emailInvalid) {
+      myErrors.push("Please provide an email.");
+    }
+    return myErrors;
+  }
+
+  public get valid(): boolean {
+    return this.errors.length === 0;
+  }
+
+  private get emailInvalid(): boolean {
+    return !BooleanHelper.hasValue(this.email);
+  }
+
+  private get passwordInvalid(): boolean {
+    return !BooleanHelper.hasValue(this.password);
+  }
+
+  public submit() {
+    this.showErrors = true;
+    if (this.valid) {
+      console.log("logging in...");
+    }
   }
 
 }
